2d6bf884184d38deb9467de1a62dd595615923e9
BHARGAVI <bhargavi07.b@gmail.com>
2025-10-14 14:18:21 +0530

    updated

--- todo-BACKEND/src/main/java/com/example/todo/ServletInitializer.java
+++ todo-BACKEND/src/main/java/com/example/todo/ServletInitializer.java
@@ -3,7 +3,7 @@ package com.example.todo;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
